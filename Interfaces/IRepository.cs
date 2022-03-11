3ee0b3430533d033461abdc98bab9e0daace2207
Gaurav Dogra <gauru.dogra@gmail.com>
2022-03-11 18:42:41 +0000

    Initial commit, with User Model

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace UserLoginApp;
+
+public interface IRepository<T, TKey>
+{
+    void Add(T entity);
+    T GetById(TKey id);
+
+    IEnumerable<T> GetAll();
+    void DeleteById(TKey id);
+
+    void Update(TKey key, T entity);
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,31 @@
+﻿using UserLoginApp;
+
+namespace UserLoginApp.Service;
+
+public class UserService : IRepository<User, int>
+{
+    public void Add(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(int key, User entity)
+    {
+        throw new NotImplementedException();
+    }
+}
